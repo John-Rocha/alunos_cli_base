3e1e65ec40f3c1af0d14f4c80eefad914c0c1afc
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-28 23:00:37 -0300

    feat: create project

---
+++ bin/alunos_cli_base.dart
@@ -0,0 +1,5 @@
+import 'package:alunos_cli_base/alunos_cli_base.dart' as alunos_cli_base;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alunos_cli_base.calculate()}!');
+}
---
+++ test/alunos_cli_base_test.dart
@@ -0,0 +1,8 @@
+import 'package:alunos_cli_base/alunos_cli_base.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
