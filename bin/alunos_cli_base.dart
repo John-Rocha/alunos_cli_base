b01e37ff12e2a0741e80fb54095f9405953595ef
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2022-12-29 00:30:04 -0300

    chore: base configs

--- bin/alunos_cli_base.dart
+++ bin/alunos_cli_base.dart
@@ -1,5 +1,40 @@
-import 'package:alunos_cli_base/alunos_cli_base.dart' as alunos_cli_base;
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${alunos_cli_base.calculate()}!');
+void main(List<String> args) {
+  // final argParser = ArgParser();
+  // argParser.addFlag('data', abbr: 'd');
+  // argParser.addOption('name', abbr: 'n');
+  // argParser.addOption('template', abbr: 't');
+  // final argResults = argParser.parse(args);
+
+  // print('${argResults['data']}');
+  // print('${argResults['name']}');
+  // print('${argResults['template']}');
+
+  CommandRunner('CLI', 'CLI')
+    ..addCommand(ExemploCommand())
+    ..run(args);
+}
+
+class ExemploCommand extends Command {
+  @override
+  String get description => 'Exemplo de comando';
+
+  @override
+  String get name => 'exemplo';
+
+  ExemploCommand() {
+    argParser.addOption(
+      'template',
+      abbr: 't',
+      help: 'Template de criação do projeto',
+    );
+  }
+
+  @override
+  void run() {
+    print(argResults?['template']);
+    print('Executar qualquer coisa!!!!');
+  }
 }
--- test/alunos_cli_base_test.dart
+++ test/alunos_cli_base_test.dart
@@ -1,8 +1,1 @@
-import 'package:alunos_cli_base/alunos_cli_base.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
