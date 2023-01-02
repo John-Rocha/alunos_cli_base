06dbde73141b7b3b41653657726676ac2dbb9808
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2023-01-02 14:19:00 -0300

    feat: add find all

---
+++ lib/src/commands/students/subcommands/find_all_command.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+
+import '../../../repositories/student_repository.dart';
+
+class FindAllCommand extends Command {
+  final StudentRepository repository;
+
+  FindAllCommand(this.repository);
+  @override
+  String get description => 'Find All Students';
+
+  @override
+  String get name => 'findAll';
+
+  @override
+  Future<void> run() async {
+    print('Aguarde buscando alunos...');
+    final students = await repository.findAll();
+    print('Apresentar também os cursos? (S ou N)');
+    final showCourses = stdin.readLineSync();
+    print('--------------');
+    print('Alunos:');
+    print('--------------');
+    for (var student in students) {
+      if (showCourses?.toLowerCase() == 's') {
+        print(
+          '${student.id} - ${student.name} ${student.courses.where(
+                (course) => course.isStudent,
+              ).map(
+                (e) => e.name,
+              ).toList()}',
+        );
+      } else if (showCourses?.toLowerCase() == 'n') {
+        print('${student.id} - ${student.name}');
+      } else {
+        print('Opção inválida!');
+        return;
+      }
+    }
+  }
+}
