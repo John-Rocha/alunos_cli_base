aaec908d1d7478ec34d9accfb3a75f832bcce929
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2023-01-02 14:18:50 -0300

    feat: add commands

--- bin/alunos_cli_base.dart
+++ bin/alunos_cli_base.dart
@@ -1,40 +1,8 @@
-import 'package:args/args.dart';
+import 'package:alunos_cli_base/src/commands/students/student_command.dart';
 import 'package:args/command_runner.dart';
 
 void main(List<String> args) {
-  // final argParser = ArgParser();
-  // argParser.addFlag('data', abbr: 'd');
-  // argParser.addOption('name', abbr: 'n');
-  // argParser.addOption('template', abbr: 't');
-  // final argResults = argParser.parse(args);
-
-  // print('${argResults['data']}');
-  // print('${argResults['name']}');
-  // print('${argResults['template']}');
-
   CommandRunner('CLI', 'CLI')
-    ..addCommand(ExemploCommand())
+    ..addCommand(StudentCommand())
     ..run(args);
-}
-
-class ExemploCommand extends Command {
-  @override
-  String get description => 'Exemplo de comando';
-
-  @override
-  String get name => 'exemplo';
-
-  ExemploCommand() {
-    argParser.addOption(
-      'template',
-      abbr: 't',
-      help: 'Template de criação do projeto',
-    );
-  }
-
-  @override
-  void run() {
-    print(argResults?['template']);
-    print('Executar qualquer coisa!!!!');
-  }
 }
---
+++ lib/src/commands/students/student_command.dart
@@ -0,0 +1,21 @@
+import 'package:args/command_runner.dart';
+
+import '../../repositories/student_repository.dart';
+import 'subcommands/find_all_command.dart';
+import 'subcommands/find_by_id_command.dart';
+import 'subcommands/insert_command.dart';
+
+class StudentCommand extends Command {
+  @override
+  String get description => 'Students Operations';
+
+  @override
+  String get name => 'students';
+
+  StudentCommand() {
+    final studentRepository = StudentRepository();
+    addSubcommand(FindAllCommand(studentRepository));
+    addSubcommand(FindByIdCommand(studentRepository));
+    addSubcommand(InsertCommand(studentRepository));
+  }
+}
